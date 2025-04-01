04fc82e676502d56b1d8ff2e4ba90b19b5adad3f
Marc <139300806+marcbensan@users.noreply.github.com>
2025-04-01 12:52:13 -0400

    feat: add unit test and e2e test in workflow

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,12 @@
+describe("check Not Found (404)", () => {
+  it("should return status 404 when visiting /unknown", () => {
+    // See Request: https://docs.cypress.io/api/commands/request and
+    // Its: https://docs.cypress.io/api/commands/its for more information regarding this test
+    cy.request({
+      url: "http://localhost:3000/unknown",
+      failOnStatusCode: false,
+    })
+      .its("status")
+      .should("equal", 404);
+  });
+});
